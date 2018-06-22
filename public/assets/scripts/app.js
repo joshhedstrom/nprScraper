293c7755b9fd13d154a2647ae1e55a3997785ca5
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-06-22 15:56:32 -0500

    scaffolded homepage, moved modules to routes page

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -1,4 +1,14 @@
+let commentsHidden = true;
 $('body').on('click', '.comment-link', e => {
-	e.preventDefault();
-	$('.all-comments').attr('style', 'display: block;');
+	console.log(this)
+    e.preventDefault();
+    if (commentsHidden) {
+        commentsHidden = false;
+        $('.all-comments').attr('style', 'display: block;');
+    }
+
+    else if (!commentsHidden) {
+    	commentsHidden = true;
+    	$('.all-comments').attr('style', 'display: none;');
+    }
 });
--- server.js
+++ server.js
@@ -1,113 +1,29 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
+const routes = require('./controllers/routes.js')
+const port = 3000;
+
 const bodyParser = require("body-parser");
 const logger = require("morgan");
+
 const mongoose = require("mongoose");
 
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-const db = require("./models");
-
-const port = 3000;
-
-const app = express();
-
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(express.static("public"));
 
-const exphbs = require("express-handlebars");
+app.use(routes)
+app.use(express.static("public"));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-
 mongoose.connect("mongodb://localhost/nprScraper");
-
-app.get('/', (req, res) => {
-    res.redirect('/articles')
-  // res.render('index')
-})
-
-app.get("/scrape", (req, res) => {
-    axios.get("http://www.npr.org/").then(response => {
-        const $ = cheerio.load(response.data);
-
-        $("div a h3").each((i, element) => {
-            let result = {};
-            result.title = $(element).text();
-            result.summary = $(element).parent().next().children('p').text()
-            result.link = $(element).parent().attr("href");
-
-            db.Article.create(result)
-                .then(dbArticle => {
-                    // console.log(dbArticle);
-                })
-                .catch(err => {
-                    console.log(`ERROR:: ${err}`);
-                });
-        });
-        res.redirect('/articles');
-    });
-});
-
-// Route for getting all Articles from the db
-app.get("/articles", (req, res) => {
-    db.Article.find({})
-        .then(articles => {
-
-            let comments = [{
-                body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum culpa tempore, voluptates vitae. Deserunt numquam distinctio esse non quod dolorem excepturi tempore error perferendis adipisci, aspernatur dolores suscipit ipsam, maiores!',
-                user: 'thisUser',
-                time: 'July 4th, 1776'
-            }]
-            let dbArticles = {
-                articles: articles,
-                comments: comments,
-            }
-          // console.log(articles)
-            res.render('index', dbArticles);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
-
-// Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
-    db.Article.findOne({
-            _id: req.params.id
-        })
-        .populate("note")
-        .then(dbArticle => {
-            res.json(dbArticle);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
-
-// Route for saving/updating an Article's associated Note
-app.post("/articles/:id", (req, res) => {
-    db.Note.create(req.body)
-        .then(dbNote => {
-            return db.Article.findOneAndUpdate({
-                _id: req.params.id
-            }, {
-                note: dbNote._id
-            }, {
-                new: true
-            });
-        })
-        .then(dbArticle => {
-            res.json(dbArticle);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
 
 app.listen(port, () => {
     console.log(`App running on http://localhost:${port}`);
