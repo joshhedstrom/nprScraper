987c739ee29155d6252b96cc9d115acfdf8c6306
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-06-22 19:59:42 -0500

    added mongoDocument id to article element

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -1,14 +1,12 @@
 let commentsHidden = true;
-$('body').on('click', '.comment-link', e => {
-	console.log(this)
+$('.comment-link').on('click', e => {
     e.preventDefault();
+    let id = e.target.nextElementSibling.id;
     if (commentsHidden) {
         commentsHidden = false;
         $('.all-comments').attr('style', 'display: block;');
-    }
-
-    else if (!commentsHidden) {
-    	commentsHidden = true;
-    	$('.all-comments').attr('style', 'display: none;');
+    } else if (!commentsHidden) {
+        commentsHidden = true;
+        $('.all-comments').attr('style', 'display: none;');
     }
 });
