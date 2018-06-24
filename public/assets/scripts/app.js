f0f9dfd96a18e04d2c2b7219bc6b8fcab928e9c9
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-06-24 15:24:56 -0500

    got comments for individual article to show

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -2,22 +2,22 @@ let commentsHidden = true;
 $('.comment-link').on('click', e => {
     e.preventDefault();
     let id = e.target.nextElementSibling.id;
-    $.ajax({
-            url: `/articles/${id}`,
-            type: 'GET',
-        })
-        .done(function(data) {
-            console.log(data)
-            console.log("success");
-        })
+    // $.ajax({
+    //         url: `/articles/${id}`,
+    //         type: 'GET',
+    //     })
+    //     .done(function(data) {
+    //         console.log(data)
+    //         console.log("success");
+    //     })
 
 
 
-    // if (commentsHidden) {
-    //     commentsHidden = false;
-    //     $('.all-comments').attr('style', 'display: block;');
-    // } else if (!commentsHidden) {
-    //     commentsHidden = true;
-    //     $('.all-comments').attr('style', 'display: none;');
-    // }
+    if (commentsHidden) {
+        commentsHidden = false;
+        $(`#${id}`).attr('style', 'display: block;');
+    } else if (!commentsHidden) {
+        commentsHidden = true;
+        $(`#${id}`).attr('style', 'display: none;');
+    }
 });
